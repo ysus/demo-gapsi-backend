6f366b7de1abfdde69257df69b0157c168d3d4b6
jesus morales panfilo <jemoral@bancolombia.com.co>
2023-10-28 14:22:35 -0600

    init commit

---
+++ src/test/java/com/gapsi/ECommerceGapsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gapsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceGapsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
